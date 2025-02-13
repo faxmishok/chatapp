c7c5e68065058fa33eb3bc53ce6c96ba20cbc1fe
Fahmin Guliyev <baku.fehmin@gmail.com>
2025-02-13 18:37:21 +0100

    changed theme

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -1,9 +1,9 @@
-import { create } from "zustand";
+import { create } from 'zustand';
 
 export const useThemeStore = create((set) => ({
-  theme: localStorage.getItem("chat-theme") || "coffee",
+  theme: localStorage.getItem('chat-theme') || 'dracula',
   setTheme: (theme) => {
-    localStorage.setItem("chat-theme", theme);
+    localStorage.setItem('chat-theme', theme);
     set({ theme });
   },
 }));
